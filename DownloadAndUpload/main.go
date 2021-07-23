63c66a094877fdd04e4b99cc102302868f27e1e0
asish_orbitax <asish.pal@orbitax.com>
2021-07-24 01:04:40 +0600

    List Item info from aws s3

--- DownloadAndUpload/main.go
+++ DownloadAndUpload/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"log"
 	"os"
@@ -28,6 +29,23 @@ func UploadItem(sess *session.Session){
 	}
 	log.Printf("uploaed file :%+v\n", result)
 }
+
+func ListItems(sess *session.Session){
+	svc:=s3.New(sess)
+	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String("go-aws-s3-first-bucket"),
+	})
+	if err != nil{
+		log.Fatal(err.Error())
+	}
+	for _,item:= range resp.Contents{
+		fmt.Println("Name:      ", *item.Key)
+		fmt.Println("Last modified:      ", *item.LastModified)
+		fmt.Println("Size:      ", *item.Size)
+		fmt.Println("Storage Class:      ", *item.StorageClass)
+		fmt.Println(" ")
+	}
+}
 func main() {
 	fmt.Println("Listening aws buckets...")
 	sess, err := session.NewSession(&aws.Config{
@@ -37,4 +55,5 @@ func main() {
 		log.Fatal(err.Error())
 	}
 	UploadItem(sess)
+	ListItems(sess)
 }
